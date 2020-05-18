c09ae49c246494e54e77afe5f390925497b9e882
Megan <meganhong@g.ucla.edu>
2020-05-18 17:16:45 +0000

    2 methods

---
+++ project4/angular-blog/src/app/blog.service.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+const jwt_decode = require('jwt_decode')
+
+export class Post {
+  postid: number;
+  created: Date;
+  modified: Date;
+  title: string;
+  body: string;
+}
+
+// function getUsername() {
+//   var str = '; ' + document.cookie;
+//   var values = str.split('; ' + 'jwt' + '=');
+//   if (values.length == 2) {
+//     var cookie = values.pop().split(';').shift();
+//   }
+//   return jwt_decode(cookie).usr;
+// }
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogService {
+  private posts: Post[];
+  constructor(private http: HttpClient) {}
+
+  fetchPosts(username: string): Promise<Post> {
+    let promise = new Promise<Post>(function(resolve, reject) {
+      this.posts = []; 
+      this.http.get('/api/' + username)
+        .subscribe(posts => this.posts = posts);
+    });
+
+    return promise;
+  }
+
+  getPost(username: string, postid: number): Promise<Post> {
+    let promise = new Promise<Post>(function(resolve, reject) {
+      this.posts = []; 
+      this.http.find(post => post.postid = postid)
+    });
+
+    return promise;
+  }
+
+  newPost(username: string, post: Post): Promise<void> {
+    let promise = new Promise<void>(function(resolve, reject) {
+      
+    });
+
+    return promise;
+  }
+
+  updatePost(username: string, post: Post): void {
+    let previous = this.posts.find((curPost) => curPost.postid == post.postid);
+    if (previous) {
+      let i = this.posts.indexOf(previous);
+      this.posts[i].title = post.title;
+      this.posts[i].body = post.body;
+      this.posts[i].modified = new Date();
+      this.http
+        .put('/api/' + username + '/' + post.postid, {
+          title: post.title,
+          body: post.body,
+        })
+        .subscribe(console.log());
+    }
+  }
+
+  deletePost(username: string, postid: number): void {
+    let post = this.posts.find((post) => post.postid == postid);
+    if (post) {
+      let i = this.posts.indexOf(post);
+      this.posts.splice(i, 1);
+      this.http
+        .delete('/api/' + username + '/' + post.postid)
+        .subscribe(console.log());
+    }
+  }
+
+  // performed as local operations, no need to implement for express server
+  // setCurrentDraft(post: Post): void {
+
+  // }
+
+  // getCurrentDraft(): Post {
+
+  // }
+}
